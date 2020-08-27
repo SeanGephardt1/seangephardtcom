a995155a5f0a6f098f3dba2db50465886ead7201
Sean Gephardt <seangephardt@live.com>
2020-08-27 15:44:27 -0700

    CSS

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import SVG from '../../art/svgs.js';
 import './home.css';
+import BtnImage1 from './ms-azure-button-image-1.png';
+import BtnImage2 from './ms-azure-button-image-1.png';
+import BtnImage3 from './ms-azure-button-image-1.png';
+
+import LinkedInIcon from '../../art/img/linkedin.png';
+import InstaGramLogo from '../../art/img/instagram.png';
 
 export default class Home extends React.Component
 {
@@ -31,9 +37,8 @@ export default class Home extends React.Component
 				<div className="home-layout-head">
 
 					<div className="home-layout-left">
-						<div className="home-blurb-header">UX Prototyping</div>
+						<div className="home-blurb-header">Designing UX Prototypes</div>
 						<div>My passion is using technology to design and develop fantastic user experiences. My passion is using technology to design and develop fantastic user experiences. My passion is using technology to design and develop fantastic user experiences. My passion is using technology to design and develop fantastic user experiences. My passion is using technology to design and develop fantastic user experiences. My passion is using technology to design and develop fantastic user experiences.</div>
-
 					</div>
 
 					<div className="home-layout-right">
@@ -50,7 +55,7 @@ export default class Home extends React.Component
 							<div className="home-section-header">Interactive Demos</div>
 							<ul className="home-ul-list">
 								<li>
-									<a href="/azure/" target="_new" title="Microsoft Azure">Microsoft Azure</a>
+									<a href="/azure/" target="_new" title="Microsoft Azure">Microsoft Azure Responsive Demo</a>
 								</li>
 								<li>
 									<a href="/movieworks/" target="_new" title="MovieWorks Demo">MovieWorks Demo</a>
@@ -67,22 +72,41 @@ export default class Home extends React.Component
 							</ul>
 						</div>
 
+						<div className="home-section">
+							<div className="home-section-header">Related Links</div>
+							<div className="home-related-link-block">
+								<a className="home-related-link" href="https://www.linkedin.com/in/seangephardt/" title="Sean Gephardt, LinkedIn">
+									<img src={LinkedInIcon} />
+									<span>LinkedIn</span>
+								</a>
+								<a className="home-related-link" href="https://github.com/SeanGephardt1" title="Sean Gephardt, Gitub">
+									{SVG.Brands.GitHub}
+									<span>GitHub</span>
+								</a>
+								<a className="home-related-link" href="https://github.com/SeanGephardt1" title="Sean Gephardt, Instagram">
+									<img src={InstaGramLogo} />
+									<span>InstaGram</span>
+								</a>
+							</div>
+						</div>
+
 					</div>
 
 				</div>
 
 
 				<div className="home-layout-body">
-					<div className="pf-block">
-						<a className="pf-block-overlay">Portfolio Example 1</a>
+					<div className="pf-block" style={{ backgroundImage: `url(${BtnImage1})` }} >
+
+						<a className="pf-block-overlay" href="/azure1/" title="Microsodt Azure">Responsive design for the Microsoft Azure Portal</a>
 					</div>
 
-					<div className="pf-block">
-						<a className="pf-block-overlay">Portfolio Example 1</a>
+					<div className="pf-block" style={{ backgroundImage: `url(${BtnImage2})` }} >
+						<a className="pf-block-overlay" href="/azure1/" title="Microsodt Azure">Portfolio Example 1</a>
 					</div>
 
-					<div className="pf-block">
-						<a className="pf-block-overlay">Portfolio Example 1</a>
+					<div className="pf-block" style={{ backgroundImage: `url(${BtnImage3})` }} >
+						<a className="pf-block-overlay" href="/azure1/" title="Microsodt Azure">Portfolio Example 1</a>
 					</div>
 
 				</div>
--- src/pages/resume/resume.js
+++ src/pages/resume/resume.js
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 import './resume.css';
 import SVG from '../../art/svgs.js';
+import LinkedInIcon from '../../art/img/linkedin.png';
 
 export default class Resume extends React.Component
 {
@@ -152,6 +153,21 @@ export default class Resume extends React.Component
 							<li>WebGL & D3.js</li>
 						</ul>
 					</div>
+
+					<div className="skills-section">
+						<div className="skills-header">Related Links</div>
+						<div className="skills-related-link-block">
+							<a className="skills-related-link" href="https://www.linkedin.com/in/seangephardt/" title="Sean Gephardt, LinkedIn">
+								<img src={LinkedInIcon} />
+								<span>LinkedIn</span>
+							</a>
+							<a className="skills-related-link" href="https://github.com/SeanGephardt1" title="Sean Gephardt, Gitub">
+								{SVG.Brands.GitHub}
+								<span>GitHub</span>
+							</a>
+						</div>
+					</div>
+
 
 
 				</div>
