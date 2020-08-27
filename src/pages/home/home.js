b741570db29590d934b2f9603c12f8abf3e9799c
Sean Gephardt <seangephardt@live.com>
2020-08-26 22:00:13 -0700

    CSS

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,9 @@
 import React from 'react';
-//	import { ThemeContext } from '../../js/theme-context.js';
 import SVG from '../../art/svgs.js';
-import LorumContextPanel from '../../controls/lorum-ipsum.js';
-
 import './home.css';
 
 export default class Home extends React.Component
 {
-    //	static contextType = ThemeContext;
 	static defaultProps = {
 		Title: "Sean Gephardt",
 		LinkTitle: "Sean Gephardt",
@@ -28,20 +24,47 @@ export default class Home extends React.Component
 		return;
     };
     render()
-    {	//  console.debug( "Home.render", this.context.theme);
+    {
         return (
 			<div className="home-layout">
+
 				<div className="home-layout-left">
-					<LorumContextPanel />
+
+
+
 				</div>
+
 				<div className="home-layout-right">
-					<div>Recent Projects</div>
-					<div>
-						<ul>
-							<li>Responsive design for http://portal.azure.com</li>
-							<li>Contribution to Microsoft Fluent Design System</li>
+
+					<div className="home-section">
+						<div className="home-section-header">Recent Projects</div>
+						<ul className="home-ul-list">
+							<li>UX interaction protoyping for <a href="http://portal.azure.com" target="_new" title="Microsoft Azure">Microsoft Azure</a></li>
+							<li>Azure IoT theming contribution to <a href="https://www.microsoft.com/design/fluent/#/web" target="_new" title="Microsoft Fluent Design System">Microsoft Fluent Design System</a></li>
 						</ul>
 					</div>
+
+					<div className="home-section">
+						<div className="home-section-header">Interactive Demos</div>
+						<ul className="home-ul-list">
+							<li>
+								<a href="/azure/" target="_new" title="Microsoft Azure">Microsoft Azure</a>
+							</li>
+							<li>
+								<a href="/movieworks/" target="_new" title="MovieWorks Demo">MovieWorks Demo</a>
+							</li>
+							<li>
+								<a href="/guitars/" target="_new" title="GuitarSeller">GuitarSeller Demo</a>
+							</li>
+							<li>
+								<a href="/webgl-demo/" target="_new" title="Web GL Demo">Web GL</a>
+							</li>
+							<li>
+								<a href="/last-fm/" target="_new" title="Last.FM Demo">Last.FM Demo</a>
+							</li>
+						</ul>
+					</div>
+
 				</div>
 			</div>
         );
