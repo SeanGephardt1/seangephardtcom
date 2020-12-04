272d070b6029cf3daaaebe692b62027a0c3f7ca4
Sean Gephardt <seangephardt@live.com>
2020-12-03 19:45:57 -0800

    SVG Demo WIP

--- src/pages/big-data/big-data-demo.js
+++ src/pages/big-data/big-data-demo.js
@@ -1,6 +1,8 @@
+//	https://raw.githubusercontent.com/petercollingridge/code-for-blog/master/svg-interaction/pan_and_zoom/pan_and_zoom.svg
+
 import React from 'react';
 import './big-data-demo.css';
-import SVG from '../../art/svgs.js';
+import TelecasterSvg from "./tele-svg.js";
 
 export default class BigDataDemo extends React.Component
 {
@@ -8,7 +10,7 @@ export default class BigDataDemo extends React.Component
 		Title: "Big Data Demo",
 		LinkTitle: "Big Data Demo",
 		Href: "/demos/big-data/",
-		Icon: SVG.AppNavButtons.About,
+		//Icon: SVG.AppNavButtons.About,
 	};
 	constructor( props )
 	{
@@ -22,16 +24,100 @@ export default class BigDataDemo extends React.Component
 
 		this.state = {
 			changed: false,
+			zoomMatrix: [1, 0, 0, 1, 0, 0],
+			transformMatrix: "matrix(1, 0, 0, 1, 0, 0)",
+			viewBoxValues: [1250, 1250],
 		};
+
+		this.ViewBox = "0 0 1250 1250";
 
 		return;
 	};
+	OnClick_ResetZoom( ev )
+	{	//	console.debug( "OnClick_ResetZoom" );
+		this.setState( {
+			zoomMatrix: [1, 0, 0, 1, 0, 0],
+			transformMatrix: "matrix(1, 0, 0, 1, 0, 0)"
+		} );
+		return;
+	}
+	Zoom( scale )
+	{
+		let _matrix_vals = this.state.zoomMatrix;
+		let _centerX = parseFloat( this.state.viewBoxValues[0] ) / 2;
+		let _centerY = parseFloat( this.state.viewBoxValues[1] ) / 2;
+
+		for ( var i = 0; i < 6; i++ )
+		{
+			_matrix_vals[i] = _matrix_vals[i] * scale;
+		}
+
+		_matrix_vals[4] += Math.round( ( ( 1 - scale ) * _centerX ) );
+		_matrix_vals[5] += Math.round( ( ( 1 - scale ) * _centerY ) );
+
+		let _t_matrix = "matrix(" + _matrix_vals.join( "," ) + ")";
+
+		this.setState( {
+			zoomMatrix: _matrix_vals,
+			transformMatrix: _t_matrix
+		} );
+	};
+	OnClick_ZoomIn( ev )
+	{	//	console.debug( "OnClick_ZoomIn", ev );
+		this.Zoom( 0.9 );
+		return;
+	};
+	OnClick_ZoomOut( ev )
+	{	//	console.debug( "OnClick_ZoomIn", ev );
+		this.Zoom( 1.1 );
+		return;
+	};
+	OnWheel_Zoom( ev )
+	{	//	console.debug( "OnWheel_Zoom", ev);
+		ev.stopPropagation();
+
+		if ( ev.shiftKey === true )
+		{
+			let _scale_val = 0;
+
+			if ( ev.deltaY === 100 )
+			{
+				_scale_val = 0.9;
+			}
+			else if ( ev.deltaY === -100 )
+			{
+				_scale_val = 1.1;
+			}
+			this.Zoom( _scale_val );
+		}
+
+		return false;
+	};
     render()
 	{
-		console.debug( "BigDataDemo.render()", this.props, this.state);
-
+		//	console.debug( "BigDataDemo.render()", this.state.transformMatrix );
         return (
-			<div className="big-data-demo-layout">Big Data Demo</div>
+			<div className="big-data-demo-layout">
+				<div className="bd-page-title">Big Data SVG Zoom App</div>
+				<div className="bd-menu">
+					<div className="bd-menu-item" onClick={ this.OnClick_ZoomIn.bind(this)}>Zoom In</div>
+					<div className="bd-menu-item" onClick={ this.OnClick_ZoomOut.bind(this)}>Zoom out</div>
+					<div className="bd-menu-item" onClick={ this.OnClick_ResetZoom.bind(this)}>Reset</div>
+				</div>
+				<svg className="bg-svg-container" x="0px" y="0px" viewBox={this.ViewBox} onWheel={ this.OnWheel_Zoom.bind(this)}>
+					<rect id="debugRect"
+						fill="rgba(0,0,255,0.01)" 
+                        x="0"
+                        y="0"
+						height={this._vb_height}
+						width={this._vb_width} />
+					{ /* content */}
+
+					<g transform={this.state.transformMatrix}>
+						<TelecasterSvg/>
+					</g>
+				</svg>
+			</div>
         );
     }
 };
