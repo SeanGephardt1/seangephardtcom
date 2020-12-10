666ee837e914ea5c112c2a761f644c0990c2f904
Sean Gephardt <seangephardt@live.com>
2020-12-09 17:32:00 -0800

    Canvas demo

--- src/pages/canvas-demo/canvas-demo.js
+++ src/pages/canvas-demo/canvas-demo.js
@@ -21,7 +21,7 @@ export default class Html5CanvasDemo extends React.Component
 
 		this.state = {
 			changed: false,
-			CanvasSize: 400
+			CanvasSize: 800
 		};
 
 		this.Canvas = React.createRef();
@@ -39,13 +39,16 @@ export default class Html5CanvasDemo extends React.Component
 		let c = this.Canvas.current.getContext( '2d' );
 		c.clearRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize );
 		c.globalAlpha = 1;
+		c.strokeStyle = "rgba(255,0,0,1)";
+		c.lineWidth = 1;
 
 		const fillGrad = c.createLinearGradient( 0, 0, 0, this.state.CanvasSize );
-		fillGrad.addColorStop("0", "rgba(0,0,128,1)");
+		fillGrad.addColorStop("0", "rgba(255,255,255,1)");
 		fillGrad.addColorStop( "1", "rgba(0,0,0,1)" );
 
 		c.fillStyle = fillGrad;
 		c.fillRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize );
+
 		c.save();
 		return c;
 	};
@@ -60,6 +63,7 @@ export default class Html5CanvasDemo extends React.Component
 		c.fillRect( 0, this.state.CanvasSize / 2, this.state.CanvasSize / 2, this.state.CanvasSize / 2 );
 		c.fillStyle = "rgba(0,96,0,1)";
 		c.fillRect( this.state.CanvasSize / 2, this.state.CanvasSize / 2, this.state.CanvasSize / 2, this.state.CanvasSize / 2 );
+		c.fillStyle = "rgba( 255, 255, 255, 1)";
 		return;
 	};
 	OnClick_CreateScaledRects()
@@ -67,8 +71,9 @@ export default class Html5CanvasDemo extends React.Component
 		let c = this.OnClick_ResetCanvas();
 		this.RenderDefaultBoxes();
 
+		c.globalAlpha = 1;
 		c.fillStyle = "rgba(255,255,255,1)";
-		c.globalAlpha = 0.375;
+		c.globalAlpha = 0.3;
 
 		for ( var i = 0; i < 5; i++ )
 		{
@@ -158,38 +163,62 @@ export default class Html5CanvasDemo extends React.Component
 	};
 	OnClick_CreateRandomGradientCircles( ev )
 	{
+		//	Note: Gradient coordinates are global
+		//	i.e., relative to the current coordinate space.
+		//	When applied to a shape, the coordinates are NOT relative to the shape's coordinates.
+		console.clear();
+
+		const debugFillStyle = "rgba(0,0,0,1)";
+		const debugStrokeStyle = "rgba(0,0,0,1)";
+			
 		let c = this.OnClick_ResetCanvas();
+		c.globalAlpha = 1;
+		c.lineWidth = 1;
 
-		// Create gradients
-		const radgrad = c.createRadialGradient(45, 45, 10, 52, 50, 30);
-		radgrad.addColorStop(0, '#A7D30C');
-		radgrad.addColorStop(0.9, '#019F62');
-		radgrad.addColorStop(1, 'rgba(1, 159, 98, 0)');
-  
-		const radgrad2 = c.createRadialGradient(105, 105, 20, 112, 120, 50);
-		radgrad2.addColorStop(0, '#FF5F98');
-		radgrad2.addColorStop(0.75, '#FF0188');
-		radgrad2.addColorStop(1, 'rgba(255, 1, 136, 0)');
+		c.shadowColor = "rgba(0,0,0,1)";
+		c.shadowBlur = 20;
+		c.shadowOffsetX = 0;
+		c.shadowOffsetY = 10;
 
-		const radgrad3 = c.createRadialGradient(95, 15, 15, 102, 20, 40);
-		radgrad3.addColorStop(0, '#00C9FF');
-		radgrad3.addColorStop(0.8, '#00B5E2');
-		radgrad3.addColorStop(1, 'rgba(0, 201, 255, 0)');
+		c.fillStyle = debugFillStyle;
+		c.strokeStyle = debugStrokeStyle;
 
-		const radgrad4 = c.createRadialGradient(0, 150, 50, 0, 140, 90);
-		radgrad4.addColorStop(0, '#F4F201');
-		radgrad4.addColorStop(0.8, '#E4C700');
-		radgrad4.addColorStop(1, 'rgba(228, 199, 0, 0)');
-  
-		// draw shapes
-		c.fillStyle = radgrad4;
-		c.fillRect(0, 0, this.state.CanvasSize, this.state.CanvasSize);
-		c.fillStyle = radgrad3;
-		c.fillRect(0, 0, this.state.CanvasSize, this.state.CanvasSize);
-		c.fillStyle = radgrad2;
-		c.fillRect(0, 0, this.state.CanvasSize, this.state.CanvasSize);
-		c.fillStyle = radgrad;
-		c.fillRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize);
+		c.fillRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize );
+
+		for ( var i = 0; i < 100; i++ )
+		{
+			let _x = Math.round( Math.random() * ( this.state.CanvasSize ) );
+			let _y = Math.round( Math.random() * ( this.state.CanvasSize ) );
+			let _r_size = Math.round( Math.random() * ( this.state.CanvasSize / 5 ) );
+
+			let _grad_x1 = _x + ( _r_size / 3 );
+			let _grad_y1 = _y - ( _r_size / 3 );
+
+			let _radius1 = 0;
+
+			const _rg = c.createRadialGradient(
+				_grad_x1, _grad_y1, _radius1,
+				_x, _y, _r_size
+			);
+
+			let _r = Math.round( Math.random() * 256 );
+			let _g = Math.round( Math.random() * 256 );
+			let _b= Math.round( Math.random() * 256 );
+
+			_rg.addColorStop( 0, "rgba(" + _r + "," + _g + "," + _b + ", 1)" );
+			_rg.addColorStop( 1, "rgba(" + (_r/5) + "," + (_g/5) + "," + (_b/5) + ", 1)" );
+
+			c.fillStyle = _rg;
+
+			c.beginPath();
+			c.arc( _x, _y, _r_size, 0, 2 * Math.PI );
+
+			c.fill();
+			c.stroke();
+		}
+
+		c.globalAlpha = 1;
+		c.save();
 		return;
 	};
     render()
@@ -199,10 +228,10 @@ export default class Html5CanvasDemo extends React.Component
 				<div className="canvas-demo-header">{this.Title }</div>
 				<div className="canvas-demo-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus. Maecenas metus lacus, consectetur quis turpis ac, sollicitudin dapibus ipsum. Maecenas hendrerit turpis a neque scelerisque rhoncus. Pellentesque lobortis arcu sed mauris porttitor, id accumsan est aliquet. Vivamus congue quam neque, ac fermentum orci rhoncus ac. Quisque in metus eros. Nullam luctus ex urna, sed bibendum metus fringilla sed.</div>
 				<div className="canvas-menu">
-					<button className="canvas-menu-btn" onClick={ this.OnClick_ResetCanvas.bind(this)}>Reset</button>
 					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateScaledRects.bind(this)}>Create scaled opaque boxes</button>
 					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateRandomArcs.bind(this)}>Create random arcs</button>
-					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateRandomGradientCircles.bind(this)}>Random circles</button>
+					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateRandomGradientCircles.bind(this)}>Gradient circles</button>
+					<button className="canvas-menu-btn" onClick={ this.OnClick_ResetCanvas.bind(this)}>Reset</button>
 				</div>
 				<div className="canvas-panel">
 					<canvas id="glcanvas" className="canvas-2d" ref={this.Canvas}
