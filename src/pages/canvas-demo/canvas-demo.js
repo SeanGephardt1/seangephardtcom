9f44807b93109c0ee189499f6d613e392c092b99
Sean Gephardt <seangephardt@live.com>
2020-12-09 18:25:48 -0800

    Canvas Demo WIP

--- src/pages/canvas-demo/canvas-demo.js
+++ src/pages/canvas-demo/canvas-demo.js
@@ -36,11 +36,18 @@ export default class Html5CanvasDemo extends React.Component
 	{
 		//	console.debug( "ResetCanvas", this.Canvas.current );
 		//	console.debug( "ResetCanvas()");
-		let c = this.Canvas.current.getContext( '2d' );
-		c.clearRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize );
+		let c = this.Canvas.current.getContext( "2d" );
 		c.globalAlpha = 1;
-		c.strokeStyle = "rgba(255,0,0,1)";
+		c.imageSmoothingQuality = "high";
 		c.lineWidth = 1;
+		c.fillStyle = "rgba(0,0,0,1)";
+		c.strokeStyle = "rgba(0,0,0,1)";
+		c.shadowColor = "rgba(0,0,0,1)";
+		c.shadowBlur = 0;
+		c.shadowOffsetX = 0;
+		c.shadowOffsetY = 0;
+
+		c.clearRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize );
 
 		const fillGrad = c.createLinearGradient( 0, 0, 0, this.state.CanvasSize );
 		fillGrad.addColorStop("0", "rgba(255,255,255,1)");
@@ -48,6 +55,8 @@ export default class Html5CanvasDemo extends React.Component
 
 		c.fillStyle = fillGrad;
 		c.fillRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize );
+
+		c.fillStyle = "rgba(255,255,255,1)";
 
 		c.save();
 		return c;
@@ -63,7 +72,7 @@ export default class Html5CanvasDemo extends React.Component
 		c.fillRect( 0, this.state.CanvasSize / 2, this.state.CanvasSize / 2, this.state.CanvasSize / 2 );
 		c.fillStyle = "rgba(0,96,0,1)";
 		c.fillRect( this.state.CanvasSize / 2, this.state.CanvasSize / 2, this.state.CanvasSize / 2, this.state.CanvasSize / 2 );
-		c.fillStyle = "rgba( 255, 255, 255, 1)";
+		c.save();
 		return;
 	};
 	OnClick_CreateScaledRects()
@@ -71,32 +80,78 @@ export default class Html5CanvasDemo extends React.Component
 		let c = this.OnClick_ResetCanvas();
 		this.RenderDefaultBoxes();
 
-		c.globalAlpha = 1;
-		c.fillStyle = "rgba(255,255,255,1)";
-		c.globalAlpha = 0.3;
-
-		for ( var i = 0; i < 5; i++ )
+		for ( var i = 0; i < 10; i++ )
 		{
-			c.beginPath();
-
 			let _new_i = i + 1;
-			c.fillRect(
-				_new_i * 20,
-				_new_i * 20,
-				this.state.CanvasSize - 40 * _new_i,
-				this.state.CanvasSize - 40 * _new_i );
 
-			c.fill();
+			c.beginPath();
+			c.globalAlpha = 0.2;
+			c.fillStyle = "rgba(255,255,255,1)";
+			c.fillRect(
+				_new_i * 40,
+				_new_i * 40,
+				this.state.CanvasSize - 80 * _new_i,
+				this.state.CanvasSize - 80 * _new_i );
 		}
 
 		c.save();
 		return;
     }
+	OnClick_CreateRandomGradientCircles( ev )
+	{
+		//	Note: Gradient coordinates are global
+		//	i.e., relative to the current coordinate space.
+		//	When applied to a shape, the coordinates are NOT relative to the shape's coordinates.
+		let c = this.OnClick_ResetCanvas();
+		this.RenderDefaultBoxes();
+
+		c.fillStyle = "rgba(0,0,0,1)";
+		c.shadowBlur = 20;
+		c.shadowOffsetY = 10;
+
+		c.fillRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize );
+
+		for ( var i = 0; i < 100; i++ )
+		{
+			let _x = Math.round( Math.random() * ( this.state.CanvasSize ) );
+			let _y = Math.round( Math.random() * ( this.state.CanvasSize ) );
+			let _r_size = Math.round( Math.random() * ( this.state.CanvasSize / 5 ) );
+
+			let _grad_x1 = _x + ( _r_size / 3 );
+			let _grad_y1 = _y - ( _r_size / 3 );
+
+			let _radius1 = 0;
+
+			const _rg = c.createRadialGradient(
+				_grad_x1, _grad_y1, _radius1,
+				_x, _y, _r_size
+			);
+
+			let _r = Math.round( Math.random() * 256 );
+			let _g = Math.round( Math.random() * 256 );
+			let _b= Math.round( Math.random() * 256 );
+
+			_rg.addColorStop( 0, "rgba(" + _r + "," + _g + "," + _b + ", 1)" );
+			_rg.addColorStop( 1, "rgba(" + (_r/5) + "," + (_g/5) + "," + (_b/5) + ", 1)" );
+
+			c.fillStyle = _rg;
+
+			c.beginPath();
+			c.arc( _x, _y, _r_size, 0, 2 * Math.PI );
+
+			c.fill();
+			c.stroke();
+		}
+
+		c.save();
+		return;
+	};
 	OnClick_CreateRandomArcs(ev)
 	{	//	console.debug( "RandomBoxSizes" );
 		//	TEST ARC
 		//	c.arc(x,y, radius, startingAngle, endingAngle, counterclockwise);
 		// cap for positive numbers using arc starting angle param is 180
+		//	https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations
 
 		let _random = Math.round( Math.random() * 360 );
 		if ( _random < 1 )
@@ -161,66 +216,6 @@ export default class Html5CanvasDemo extends React.Component
 		c.save();
 		return;
 	};
-	OnClick_CreateRandomGradientCircles( ev )
-	{
-		//	Note: Gradient coordinates are global
-		//	i.e., relative to the current coordinate space.
-		//	When applied to a shape, the coordinates are NOT relative to the shape's coordinates.
-		console.clear();
-
-		const debugFillStyle = "rgba(0,0,0,1)";
-		const debugStrokeStyle = "rgba(0,0,0,1)";
-			
-		let c = this.OnClick_ResetCanvas();
-		c.globalAlpha = 1;
-		c.lineWidth = 1;
-
-		c.shadowColor = "rgba(0,0,0,1)";
-		c.shadowBlur = 20;
-		c.shadowOffsetX = 0;
-		c.shadowOffsetY = 10;
-
-		c.fillStyle = debugFillStyle;
-		c.strokeStyle = debugStrokeStyle;
-
-		c.fillRect( 0, 0, this.state.CanvasSize, this.state.CanvasSize );
-
-		for ( var i = 0; i < 100; i++ )
-		{
-			let _x = Math.round( Math.random() * ( this.state.CanvasSize ) );
-			let _y = Math.round( Math.random() * ( this.state.CanvasSize ) );
-			let _r_size = Math.round( Math.random() * ( this.state.CanvasSize / 5 ) );
-
-			let _grad_x1 = _x + ( _r_size / 3 );
-			let _grad_y1 = _y - ( _r_size / 3 );
-
-			let _radius1 = 0;
-
-			const _rg = c.createRadialGradient(
-				_grad_x1, _grad_y1, _radius1,
-				_x, _y, _r_size
-			);
-
-			let _r = Math.round( Math.random() * 256 );
-			let _g = Math.round( Math.random() * 256 );
-			let _b= Math.round( Math.random() * 256 );
-
-			_rg.addColorStop( 0, "rgba(" + _r + "," + _g + "," + _b + ", 1)" );
-			_rg.addColorStop( 1, "rgba(" + (_r/5) + "," + (_g/5) + "," + (_b/5) + ", 1)" );
-
-			c.fillStyle = _rg;
-
-			c.beginPath();
-			c.arc( _x, _y, _r_size, 0, 2 * Math.PI );
-
-			c.fill();
-			c.stroke();
-		}
-
-		c.globalAlpha = 1;
-		c.save();
-		return;
-	};
     render()
 	{
         return (
@@ -228,13 +223,13 @@ export default class Html5CanvasDemo extends React.Component
 				<div className="canvas-demo-header">{this.Title }</div>
 				<div className="canvas-demo-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus. Maecenas metus lacus, consectetur quis turpis ac, sollicitudin dapibus ipsum. Maecenas hendrerit turpis a neque scelerisque rhoncus. Pellentesque lobortis arcu sed mauris porttitor, id accumsan est aliquet. Vivamus congue quam neque, ac fermentum orci rhoncus ac. Quisque in metus eros. Nullam luctus ex urna, sed bibendum metus fringilla sed.</div>
 				<div className="canvas-menu">
-					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateScaledRects.bind(this)}>Create scaled opaque boxes</button>
-					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateRandomArcs.bind(this)}>Create random arcs</button>
-					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateRandomGradientCircles.bind(this)}>Gradient circles</button>
+					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateScaledRects.bind(this)}>Render scaled opaque boxes</button>
+					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateRandomGradientCircles.bind(this)}>Render gradient circles</button>
+					<button className="canvas-menu-btn" onClick={ this.OnClick_CreateRandomArcs.bind(this)}>Render animated</button>
 					<button className="canvas-menu-btn" onClick={ this.OnClick_ResetCanvas.bind(this)}>Reset</button>
 				</div>
 				<div className="canvas-panel">
-					<canvas id="glcanvas" className="canvas-2d" ref={this.Canvas}
+					<canvas id="html-5-canvas" className="canvas-2d" ref={this.Canvas}
 						height={this.state.CanvasSize} width={this.state.CanvasSize}></canvas>
 				</div>					
 			</div>
