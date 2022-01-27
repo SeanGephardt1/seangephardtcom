688640dd51dffd80dfe4930c3b1a5a852e7940f0
Sean Gephardt <22643339+SeanGephardt1@users.noreply.github.com>
2022-01-26 21:20:33 -0800

    EOD checkin - repeat steps for #dev #deploy, move delay for main

--- src/controls/home-svg/home-svg.js
+++ src/controls/home-svg/home-svg.js
@@ -23,6 +23,12 @@ export default class HomeSvg extends React.Component
 				height="540"
 				viewBox="0 0 1920 1080"
 			>
+				<defs>
+					<filter id="design-asset-shadow">
+						<feDropShadow dx="0" dy="4" stdDeviation="8" floodColor="rgba(0,0,0,1)" floodOpacity="0.6"  />
+					</filter>
+				</defs>
+
 				{  /* DEBUG BACKGROUND */
 					this.state.debug === true &&
 					<g className="home-svg-debug">
@@ -36,11 +42,11 @@ export default class HomeSvg extends React.Component
 							<circle r="30" cx="50%" cy="50%" />
 						</g>
 
-						{/*<rect className="palette-rect-1" x="710" />*/}
-						{/*<rect className="palette-rect-2" x="810" />*/}
-						{/*<rect className="palette-rect-3" x="910" />*/}
-						{/*<rect className="palette-rect-4" x="1010" />*/}
-						{/*<rect className="palette-rect-5" x="1110" />*/}
+						<rect className="palette-rect-1" x="710" />
+						<rect className="palette-rect-2" x="810" />
+						<rect className="palette-rect-3" x="910" />
+						<rect className="palette-rect-4" x="1010" />
+						<rect className="palette-rect-5" x="1110" />
 					</g>
 				}
 
@@ -93,7 +99,94 @@ export default class HomeSvg extends React.Component
 						letterSpacing="-32px">
 						<tspan dx="0" dy="0">#design</tspan>
 					</text>
-					<path className="design-asset-path-test" d="M100,100 L100,100" />
+
+					<g className="g-design-flow">
+						<rect
+							className="design-rect dr-layout"
+							x="200"
+							y="300"
+							width="500"
+							height="200"
+							rx="6"
+							filter="url(#design-asset-shadow)" />
+
+						<rect
+							className="design-rect dr-toolbar"
+							x="200"
+							y="250"
+							width="1520"
+							height="80"
+							rx="2" />
+						<text className="design-text dr-brand" x="222" y="302">Company Brand</text>
+						<text className="design-text dr-login" x="1620" y="302">Login</text>
+						<text className="design-text dr-content-text" x="500" y="400">Welcome Valued Customer!</text>
+						<text className="design-text dr-content-text-2" x="500" y="460">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>
+						<text className="design-text dr-content-text-2" x="500" y="490">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>
+						<text className="design-text dr-content-text-2" x="500" y="520">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>
+						<text className="design-text dr-content-text-2" x="500" y="550">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>
+
+						<g className="g-dr-menu">
+							<rect
+								className="design-rect dr-menu"
+								x="200"
+								y="332"
+								width="250"
+								height="718"
+								rx="0" />
+
+								<rect
+									className="dr-menu-icon"
+									x="220"
+									y="360"
+									width="32"
+									height="32"
+									rx="5" />
+								<rect
+									className="dr-menu-icon"
+									x="220"
+									y="430"
+									width="32"
+									height="32"
+									rx="5" />
+								<rect
+									className="dr-menu-icon"
+									x="220"
+									y="500"
+									width="32"
+									height="32"
+									rx="5" />
+								<rect
+									className="dr-menu-icon"
+									x="220"
+									y="570"
+									width="32"
+									height="32"
+									rx="5" />
+
+								<text
+									className="dr-menu-icon-text"
+									x="270"
+									y="388"
+								>Feature One</text>
+								<text
+									className="dr-menu-icon-text"
+									x="270"
+									y="454"
+								>Feature Two</text>
+								<text
+									className="dr-menu-icon-text"
+									x="270"
+									y="528"
+								>Feature Three</text>
+								<text
+									className="dr-menu-icon-text"
+									x="270"
+									y="598"
+								>Feature Four</text>
+
+						</g>
+
+					</g>
 				</g>
 
 				{/*<g id="develop-flow">*/}
