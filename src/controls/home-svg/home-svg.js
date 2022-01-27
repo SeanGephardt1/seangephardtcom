179b49803c1207c967d03291898777985b738ffb
Sean Gephardt <22643339+SeanGephardt1@users.noreply.github.com>
2022-01-26 22:24:25 -0800

    EOD

--- src/controls/home-svg/home-svg.js
+++ src/controls/home-svg/home-svg.js
@@ -6,7 +6,7 @@ export default class HomeSvg extends React.Component
 	constructor ( props ) 
 	{
 		super( props );
-		this.state = { debug: true };
+		this.state = { debug: false };
 		return;
 	};
 	render()
@@ -17,13 +17,16 @@ export default class HomeSvg extends React.Component
 				xmlns="http://www.w3.org/2000/svg"
 				xmlnsXlink="http://www.w3.org/1999/xlink"
 				xmlSpace="preserve"
-				className="home-svg-root"
 				textRendering="optimizeLegibility"
-				preserveAspectRatio="xMinYMin slice"
-				vectorEffect="non-scaling-stroke"
-				x="0px" y="0px"
 				viewBox="0 0 1920 1080"
+				className="home-svg-root"
 			>
+				<defs>
+					<filter id="design-asset-shadow">
+						<feDropShadow dx="0" dy="4" stdDeviation="8" floodColor="rgba(0,0,0,1)" floodOpacity="0.6"  />
+					</filter>
+				</defs>
+
 				{  /* DEBUG BACKGROUND */
 					this.state.debug === true &&
 					<g className="home-svg-debug">
@@ -37,11 +40,11 @@ export default class HomeSvg extends React.Component
 							<circle r="30" cx="50%" cy="50%" />
 						</g>
 
-						{/*<rect className="palette-rect-1" x="710" />*/}
-						{/*<rect className="palette-rect-2" x="810" />*/}
-						{/*<rect className="palette-rect-3" x="910" />*/}
-						{/*<rect className="palette-rect-4" x="1010" />*/}
-						{/*<rect className="palette-rect-5" x="1110" />*/}
+						<rect className="palette-rect-1" x="710" />
+						<rect className="palette-rect-2" x="810" />
+						<rect className="palette-rect-3" x="910" />
+						<rect className="palette-rect-4" x="1010" />
+						<rect className="palette-rect-5" x="1110" />
 					</g>
 				}
 
@@ -49,34 +52,35 @@ export default class HomeSvg extends React.Component
 				<g className="home-svg-main-ux">
 					<text className="home-text-UX"
 						x="50%"
-						y="60%"
+						y="640"
 						fontFamily="Segoe UI"
-						fontSize="600"
-						textAnchor="middle">
-						<tspan>UX</tspan>
+						fontSize="600px"
+						textAnchor="middle"
+						letterSpacing="-66px">
+						<tspan dx="-48px" dy="0">UX</tspan>
 					</text>
-				</g>
-
-				<g>
 					<text className="home-text-DESIGN"
 						x="50%"
-						y="75%"
+						y="794"
+						dx="0"
+						dy="0"
 						fontFamily="Segoe UI"
-						fontSize="202"
+						fontSize="202px"
 						textAnchor="middle"
-						>
+						letterSpacing="-6px">
 						<tspan>DESIGN</tspan>
 					</text>
-				</g>
-
-				<g>
 					<text
 						className="home-text-PROTOTPYING"
 						x="50%"
-						y="83%"
+						y="884"
+						dx="0"
+						dy="0"
 						fontFamily="Segoe UI"
-						fontSize="106"
-						textAnchor="middle">
+						fontSize="108px"
+						textLength="0"
+						textAnchor="middle"
+						letterSpacing="-4px">
 						<tspan>PROTOTYPING</tspan>
 					</text>
 				</g>
