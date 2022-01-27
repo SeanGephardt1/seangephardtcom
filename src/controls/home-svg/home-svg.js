fd64e324e008e471f9afac2f351d066c5cc26491
Sean Gephardt <22643339+SeanGephardt1@users.noreply.github.com>
2022-01-26 21:41:07 -0800

    WORKING FIXES FOR MOBILE BROWSERS

--- src/controls/home-svg/home-svg.js
+++ src/controls/home-svg/home-svg.js
@@ -19,8 +19,8 @@ export default class HomeSvg extends React.Component
 				xmlSpace="preserve"
 				className="home-svg-root"
 				textRendering="optimizeLegibility"
-				width="960"
-				height="540"
+				width="960px"
+				height="540px"
 				viewBox="0 0 1920 1080"
 			>
 				<defs>
@@ -88,106 +88,106 @@ export default class HomeSvg extends React.Component
 				</g>
 
 				{ /* RANDOM DESIGN ELEMENTS */ }
-				<g id="design-flow">
-					<text
-						x="50%"
-						y="60%"
-						className="design-asset-text"
-						fontFamily="Segoe UI"
-						fontSize="400px"
-						textAnchor="middle"
-						letterSpacing="-32px">
-						<tspan dx="0" dy="0">#design</tspan>
-					</text>
+				{/*<g id="design-flow">*/}
+				{/*	<text*/}
+				{/*		x="50%"*/}
+				{/*		y="60%"*/}
+				{/*		className="design-asset-text"*/}
+				{/*		fontFamily="Segoe UI"*/}
+				{/*		fontSize="400px"*/}
+				{/*		textAnchor="middle"*/}
+				{/*		letterSpacing="-32px">*/}
+				{/*		<tspan dx="0" dy="0">#design</tspan>*/}
+				{/*	</text>*/}
 
-					<g className="g-design-flow">
-						<rect
-							className="design-rect dr-layout"
-							x="200"
-							y="300"
-							width="500"
-							height="200"
-							rx="6"
-							filter="url(#design-asset-shadow)" />
+				{/*	<g className="g-design-flow">*/}
+				{/*		<rect*/}
+				{/*			className="design-rect dr-layout"*/}
+				{/*			x="200"*/}
+				{/*			y="300"*/}
+				{/*			width="500"*/}
+				{/*			height="200"*/}
+				{/*			rx="6"*/}
+				{/*			filter="url(#design-asset-shadow)" />*/}
 
-						<rect
-							className="design-rect dr-toolbar"
-							x="200"
-							y="250"
-							width="1520"
-							height="80"
-							rx="2" />
-						<text className="design-text dr-brand" x="222" y="302">Company Brand</text>
-						<text className="design-text dr-login" x="1620" y="302">Login</text>
-						<text className="design-text dr-content-text" x="500" y="400">Welcome Valued Customer!</text>
-						<text className="design-text dr-content-text-2" x="500" y="460">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>
-						<text className="design-text dr-content-text-2" x="500" y="490">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>
-						<text className="design-text dr-content-text-2" x="500" y="520">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>
-						<text className="design-text dr-content-text-2" x="500" y="550">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>
+				{/*		<rect*/}
+				{/*			className="design-rect dr-toolbar"*/}
+				{/*			x="200"*/}
+				{/*			y="250"*/}
+				{/*			width="1520"*/}
+				{/*			height="80"*/}
+				{/*			rx="2" />*/}
+				{/*		<text className="design-text dr-brand" x="222" y="302">Company Brand</text>*/}
+				{/*		<text className="design-text dr-login" x="1620" y="302">Login</text>*/}
+				{/*		<text className="design-text dr-content-text" x="500" y="400">Welcome Valued Customer!</text>*/}
+				{/*		<text className="design-text dr-content-text-2" x="500" y="460">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>*/}
+				{/*		<text className="design-text dr-content-text-2" x="500" y="490">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>*/}
+				{/*		<text className="design-text dr-content-text-2" x="500" y="520">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>*/}
+				{/*		<text className="design-text dr-content-text-2" x="500" y="550">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at faucibus felis. Nulla faucibus elit vel mollis finibus.</text>*/}
 
-						<g className="g-dr-menu">
-							<rect
-								className="design-rect dr-menu"
-								x="200"
-								y="332"
-								width="250"
-								height="718"
-								rx="0" />
+				{/*		<g className="g-dr-menu">*/}
+				{/*			<rect*/}
+				{/*				className="design-rect dr-menu"*/}
+				{/*				x="200"*/}
+				{/*				y="332"*/}
+				{/*				width="250"*/}
+				{/*				height="718"*/}
+				{/*				rx="0" />*/}
 
-								<rect
-									className="dr-menu-icon"
-									x="220"
-									y="360"
-									width="32"
-									height="32"
-									rx="5" />
-								<rect
-									className="dr-menu-icon"
-									x="220"
-									y="430"
-									width="32"
-									height="32"
-									rx="5" />
-								<rect
-									className="dr-menu-icon"
-									x="220"
-									y="500"
-									width="32"
-									height="32"
-									rx="5" />
-								<rect
-									className="dr-menu-icon"
-									x="220"
-									y="570"
-									width="32"
-									height="32"
-									rx="5" />
+				{/*				<rect*/}
+				{/*					className="dr-menu-icon"*/}
+				{/*					x="220"*/}
+				{/*					y="360"*/}
+				{/*					width="32"*/}
+				{/*					height="32"*/}
+				{/*					rx="5" />*/}
+				{/*				<rect*/}
+				{/*					className="dr-menu-icon"*/}
+				{/*					x="220"*/}
+				{/*					y="430"*/}
+				{/*					width="32"*/}
+				{/*					height="32"*/}
+				{/*					rx="5" />*/}
+				{/*				<rect*/}
+				{/*					className="dr-menu-icon"*/}
+				{/*					x="220"*/}
+				{/*					y="500"*/}
+				{/*					width="32"*/}
+				{/*					height="32"*/}
+				{/*					rx="5" />*/}
+				{/*				<rect*/}
+				{/*					className="dr-menu-icon"*/}
+				{/*					x="220"*/}
+				{/*					y="570"*/}
+				{/*					width="32"*/}
+				{/*					height="32"*/}
+				{/*					rx="5" />*/}
 
-								<text
-									className="dr-menu-icon-text"
-									x="270"
-									y="388"
-								>Feature One</text>
-								<text
-									className="dr-menu-icon-text"
-									x="270"
-									y="454"
-								>Feature Two</text>
-								<text
-									className="dr-menu-icon-text"
-									x="270"
-									y="528"
-								>Feature Three</text>
-								<text
-									className="dr-menu-icon-text"
-									x="270"
-									y="598"
-								>Feature Four</text>
+				{/*				<text*/}
+				{/*					className="dr-menu-icon-text"*/}
+				{/*					x="270"*/}
+				{/*					y="388"*/}
+				{/*				>Feature One</text>*/}
+				{/*				<text*/}
+				{/*					className="dr-menu-icon-text"*/}
+				{/*					x="270"*/}
+				{/*					y="454"*/}
+				{/*				>Feature Two</text>*/}
+				{/*				<text*/}
+				{/*					className="dr-menu-icon-text"*/}
+				{/*					x="270"*/}
+				{/*					y="528"*/}
+				{/*				>Feature Three</text>*/}
+				{/*				<text*/}
+				{/*					className="dr-menu-icon-text"*/}
+				{/*					x="270"*/}
+				{/*					y="598"*/}
+				{/*				>Feature Four</text>*/}
 
-						</g>
+				{/*		</g>*/}
 
-					</g>
-				</g>
+				{/*	</g>*/}
+				{/*</g>*/}
 
 				{/*<g id="develop-flow">*/}
 				{/*	<text*/}
