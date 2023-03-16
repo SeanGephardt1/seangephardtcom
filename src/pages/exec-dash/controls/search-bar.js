ec0de35da61ab7cf986a12acaf5cd5a2e914bf56
Sean Gephardt <22643339+SeanGephardt1@users.noreply.github.com>
2023-03-15 19:41:38 -0700

    wip

--- src/pages/exec-dash/controls/search-bar.js
+++ src/pages/exec-dash/controls/search-bar.js
@@ -59,11 +59,18 @@ export default class ExecSearchBar extends React.Component
           <button
             tabIndex="0"
             className="esp-input-btn"
-            onClick={ this.OnClick_Enable_Voice.bind( this ) }>V</button>
+            onClick={ this.OnClick_Enable_Voice.bind( this ) }>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+              <path d="M176 0C123 0 80 43 80 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM48 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H104c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H200V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z" /></svg>
+          </button>
           <button
             tabIndex="0"
             className="esp-input-btn"
-            onClick={ this.OnClick_Submit_Search.bind(this)}>S</button>
+            onClick={ this.OnClick_Submit_Search.bind( this ) }>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+            </svg>
+          </button>
         </div>
         <div className="esp-results-panel">
           <div>result</div>
